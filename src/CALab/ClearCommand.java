7c202711dab1ba274fd32ba1082efaa5f76aff31
Ethan Le <ethanisverybusyrightnow@gmail.com>
2024-03-14 11:16:49 -0700

    Created All Of The Commands

--- src/CALab/ClearCommand.java
+++ src/CALab/ClearCommand.java
@@ -1,4 +1,18 @@
 package CALab;
 
-public class ClearCommand {
+import mvc.Command;
+import mvc.Model;
+
+public class ClearCommand extends Command {
+    public ClearCommand(Model model) {
+        super(model);
+    }
+
+    public void execute() throws Exception {
+        if (!(model instanceof Grid)) {
+            throw new Exception("Model must instantiate Grid");
+        }
+        Grid grid = (Grid) model;
+        grid.repopulate(false);
+    }
 }
