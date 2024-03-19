c7549aee95d7c3d05b6221cb24381c50fda82b30
Ethan Le <ethanisverybusyrightnow@gmail.com>
2024-03-18 21:24:40 -0700

    Refactored GridView

--- src/CALab/GridView.java
+++ src/CALab/GridView.java
@@ -2,23 +2,43 @@ package CALab;
 
 import mvc.*;
 
+import java.awt.*;
+
 public class GridView extends View {
 
     private CellView cellViews[][];
+    int dim;
 
     public GridView(Model model) {
         super(model);
-        for (int i = 0; i < ((Grid) model).getDim(); i++) {
-            for (int j = 0; j < ((Grid) model).getDim(); i++) {
+        setGrid();
+    }
+
+    @Override
+    public void setView(Model model) {
+        super.setView(model);
+        this.removeAll();
+        setGrid();
+    }
+
+    public void setGrid() {
+        dim = ((Grid) model).getDim();
+        this.setLayout(new GridLayout(dim, dim));
+        cellViews = new CellView[dim][dim];
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
                 CellView cell = new CellView(((Grid)model).getCell(i, j));
                 cellViews[i][j] = cell;
+                cellViews[i][j].setOpaque(true);
+                this.add(cellViews[i][j]);
+                cellViews[i][j].update();
             }
         }
     }
-
+    @Override
     public void update() {
-        for (int i = 0; i < ((Grid) model).getDim(); i++) {
-            for (int j = 0; j < ((Grid) model).getDim(); i++) {
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
                 cellViews[i][j].update();
             }
         }
