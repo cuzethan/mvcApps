a6fe29df38d5a7b2ed4fad6de5c80828a029fc9e
Ethan Le <ethanisverybusyrightnow@gmail.com>
2024-03-20 20:59:10 -0700

    Kaizan's implementation of button

--- src/CALab/GridPanel.java
+++ src/CALab/GridPanel.java
@@ -1,6 +1,5 @@
 package CALab;
 
-import LifeLab.LifeFactory;
 import mvc.*;
 
 import javax.swing.*;
@@ -15,54 +14,38 @@ public class GridPanel extends AppPanel {
     public GridPanel(AppFactory f){
         super(f);
 
-        controlPanel.setLayout(new GridLayout(1,2));
+        controlPanel.setLayout(new GridLayout(2, 2));
 
-        JPanel leftPanel = new JPanel();
-        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.PAGE_AXIS));
+        JPanel run1Panel = new JPanel(new FlowLayout());
+        JPanel run50Panel = new JPanel(new FlowLayout());
+        JPanel populatePanel = new JPanel(new FlowLayout());
+        JPanel clearPanel = new JPanel(new FlowLayout());
 
-        JPanel rightPanel = new JPanel();
-        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.PAGE_AXIS));
+        run1Panel.setBackground(Color.PINK);
+        run50Panel.setBackground(Color.PINK);
+        populatePanel.setBackground(Color.PINK);
+        clearPanel.setBackground(Color.PINK);
+
 
         run1 = new JButton("Run1");
-        run1.addActionListener(this);
-        run1.setAlignmentX(Component.CENTER_ALIGNMENT);
-        run1.setMaximumSize(run1.getPreferredSize());
-
         run50 = new JButton("Run50");
-        run50.addActionListener(this);
-        run50.setAlignmentX(Component.CENTER_ALIGNMENT);
-        run50.setMaximumSize(run50.getPreferredSize());
-
         populate = new JButton("Populate");
-        populate.addActionListener(this);
-        populate.setAlignmentX(Component.CENTER_ALIGNMENT);
-        populate.setMaximumSize(populate.getPreferredSize());
-
-
         clear = new JButton("Clear");
+
+        run1Panel.add(run1);
+        run50Panel.add(run50);
+        populatePanel.add(populate);
+        clearPanel.add(clear);
+
+        controlPanel.add(run1Panel);
+        controlPanel.add(run50Panel);
+        controlPanel.add(populatePanel);
+        controlPanel.add(clearPanel);
+
+        run1.addActionListener(this);
+        run50.addActionListener(this);
+        populate.addActionListener(this);
         clear.addActionListener(this);
-        clear.setAlignmentX(Component.CENTER_ALIGNMENT);
-        clear.setMaximumSize(clear.getPreferredSize());
-
-        leftPanel.add(Box.createRigidArea(new Dimension(0, 5)));
-        leftPanel.add(run1);
-        leftPanel.add(Box.createRigidArea(new Dimension(0, 150)));
-        leftPanel.add(populate);
-
-        rightPanel.add(Box.createRigidArea(new Dimension(0, 5)));
-        rightPanel.add(run50);
-        rightPanel.add(Box.createRigidArea(new Dimension(0, 150)));
-        rightPanel.add(clear);
-
-
-        controlPanel.add(leftPanel);
-        controlPanel.add(rightPanel);
-
-        /**
-        controlPanel.add(run1);
-        controlPanel.add(run50);
-        controlPanel.add(populate);
-        controlPanel.add(clear);*/
     }
 
 }
