b66e29823337324ee76ee9b8b1555f032e438875
KaizanS <kdsatta03@gmail.com>
2024-03-20 20:07:05 -0700

    Fixed buttons

--- src/CALab/GridPanel.java
+++ src/CALab/GridPanel.java
@@ -1,8 +1,10 @@
 package CALab;
 
+import LifeLab.LifeFactory;
 import mvc.*;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GridPanel extends AppPanel {
     private JButton run1;
@@ -10,31 +12,48 @@ public class GridPanel extends AppPanel {
     private JButton populate;
     private JButton clear;
 
-    public GridPanel(GridFactory f){
+    public GridPanel(AppFactory f){
         super(f);
+
+        controlPanel.setLayout(new GridLayout(2, 2));
+
+        JPanel run1Panel = new JPanel(new FlowLayout());
+        JPanel run50Panel = new JPanel(new FlowLayout());
+        JPanel populatePanel = new JPanel(new FlowLayout());
+        JPanel clearPanel = new JPanel(new FlowLayout());
+
+        run1Panel.setBackground(Color.PINK);
+        run50Panel.setBackground(Color.PINK);
+        populatePanel.setBackground(Color.PINK);
+        clearPanel.setBackground(Color.PINK);
+
+
         run1 = new JButton("Run1");
-        run1.addActionListener(this);
-
         run50 = new JButton("Run50");
-        run50.addActionListener(this);
-
-
         populate = new JButton("Populate");
+        clear = new JButton("Clear");
+
+
+        run1Panel.add(run1);
+        run50Panel.add(run50);
+        populatePanel.add(populate);
+        clearPanel.add(clear);
+
+        controlPanel.add(run1Panel);
+        controlPanel.add(run50Panel);
+        controlPanel.add(populatePanel);
+        controlPanel.add(clearPanel);
+
+
+        run1.addActionListener(this);
+        run50.addActionListener(this);
         populate.addActionListener(this);
-
-        clear = new JButton("clear");
         clear.addActionListener(this);
-
-        controlPanel.add(run1);
-        controlPanel.add(run50);
-        controlPanel.add(populate);
-        controlPanel.add(clear);
     }
-    /**
-    public static void main(String[] args) {
 
-        AppFactory factory = new GridFactory();
+    public static void main(String[] args) {
+        AppFactory factory = new LifeFactory();
         AppPanel panel = new GridPanel(factory);
         panel.display();
-    }*/
+    }
 }
